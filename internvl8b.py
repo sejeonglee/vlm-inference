4e6b5dd051ba209e32b35a04159db3a4e8519eed
sejeonglee <sejeonglee2@gmail.com>
2024-08-07 09:17:49 +0900

    feat: add internvl8b

--- internvl8b.py
+++ internvl8b.py
@@ -2,59 +2,140 @@ from typing import Final
 from transformers import (
     AutoTokenizer,
     AutoModel,
-    Intern
-    
-    ProcessorMixin,
     PreTrainedModel,
 )
 import torch
 from PIL import Image
+import torchvision.transforms as T
+from torchvision.transforms.functional import InterpolationMode
 
 
-class LLaVANext8BInference:
+def build_transform(input_size):
+    IMAGENET_MEAN = (0.485, 0.456, 0.406)
+    IMAGENET_STD = (0.229, 0.224, 0.225)
+    MEAN, STD = IMAGENET_MEAN, IMAGENET_STD
+    transform = T.Compose(
+        [
+            T.Lambda(lambda img: img.convert("RGB") if img.mode != "RGB" else img),
+            T.Resize((input_size, input_size), interpolation=InterpolationMode.BICUBIC),
+            T.ToTensor(),
+            T.Normalize(mean=MEAN, std=STD),
+        ]
+    )
+    return transform
+
+
+def find_closest_aspect_ratio(aspect_ratio, target_ratios, width, height, image_size):
+    best_ratio_diff = float("inf")
+    best_ratio = (1, 1)
+    area = width * height
+    for ratio in target_ratios:
+        target_aspect_ratio = ratio[0] / ratio[1]
+        ratio_diff = abs(aspect_ratio - target_aspect_ratio)
+        if ratio_diff < best_ratio_diff:
+            best_ratio_diff = ratio_diff
+            best_ratio = ratio
+        elif ratio_diff == best_ratio_diff:
+            if area > 0.5 * image_size * image_size * ratio[0] * ratio[1]:
+                best_ratio = ratio
+    return best_ratio
+
+
+def dynamic_preprocess(
+    image, min_num=1, max_num=12, image_size=448, use_thumbnail=False
+):
+    orig_width, orig_height = image.size
+    aspect_ratio = orig_width / orig_height
+
+    # calculate the existing image aspect ratio
+    target_ratios = set(
+        (i, j)
+        for n in range(min_num, max_num + 1)
+        for i in range(1, n + 1)
+        for j in range(1, n + 1)
+        if i * j <= max_num and i * j >= min_num
+    )
+    target_ratios = sorted(target_ratios, key=lambda x: x[0] * x[1])
+
+    # find the closest aspect ratio to the target
+    target_aspect_ratio = find_closest_aspect_ratio(
+        aspect_ratio, target_ratios, orig_width, orig_height, image_size
+    )
+
+    # calculate the target width and height
+    target_width = image_size * target_aspect_ratio[0]
+    target_height = image_size * target_aspect_ratio[1]
+    blocks = target_aspect_ratio[0] * target_aspect_ratio[1]
+
+    # resize the image
+    resized_img = image.resize((target_width, target_height))
+    processed_images = []
+    for i in range(blocks):
+        box = (
+            (i % (target_width // image_size)) * image_size,
+            (i // (target_width // image_size)) * image_size,
+            ((i % (target_width // image_size)) + 1) * image_size,
+            ((i // (target_width // image_size)) + 1) * image_size,
+        )
+        # split the image
+        split_img = resized_img.crop(box)
+        processed_images.append(split_img)
+    assert len(processed_images) == blocks
+    if use_thumbnail and len(processed_images) != 1:
+        thumbnail_img = image.resize((image_size, image_size))
+        processed_images.append(thumbnail_img)
+    return processed_images
+
+
+class InternVL8BInference:
     """
-    Requires 19.5GB for cuda GPU
+    Requires 19GB for cuda GPU
     """
 
     device: torch.device
-    model_name: Final[str]7 = "llava-hf/llama3-llava-next-8b-hf"
-    processor: LlavaNextProcessor
+    model_name: Final[str] = "OpenGVLab/InternVL2-8B"
+    tokenizer: AutoTokenizer
     model: PreTrainedModel
+    input_size: Final[int] = 448
+    max_num: Final[int] = 12
 
     def __init__(self, device: torch.device | str):
         if isinstance(device, str):
             device = torch.device(device)
         self.device = device
+        
+        torch.cuda.set_device(self.device)
 
-        processor = LlavaNextProcessor.from_pretrained(self.model_name)
-        self.processor = (
-            processor if isinstance(processor, ProcessorMixin) else processor[0]
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name, trust_remote_code=True, use_fast=False
         )
 
-        self.model = LlavaNextForConditionalGeneration.from_pretrained(
+        self.model = AutoModel.from_pretrained(
             self.model_name,
-            torch_dtype=torch.float16,
-            low_cpu_mem_usage=
-        ).to(
-            self.device  # type: ignore
-        )
+            torch_dtype=torch.bfloat16,
+            low_cpu_mem_usage=True,
+            trust_remote_code=True,
+            device_map=device,
+        ).eval()
 
     def __call__(self, query: str, image: Image.Image):
         return self.query_for_image(query, image)
 
-    def query_for_image(self, query: str, image: Image.Image) -> str:
-        conversation = [
-            {
-                "role": "user",
-                "content": [{"type": "image"}, {"type": "text", "text": query}],
-            }
-        ]
-
-        prompt = self.processor.apply_chat_template(
-            conversation, add_generation_prompt=True
+    def _preprocess_image(self, image: Image.Image) -> torch.Tensor:
+        transform = build_transform(input_size=self.input_size)
+        images = dynamic_preprocess(
+            image, image_size=self.input_size, use_thumbnail=True, max_num=self.max_num
         )
-        inputs = self.processor(prompt, image, return_tensors="pt").to(self.device)
+        pixel_values = [transform(image) for image in images]
+        pixel_values = torch.stack(pixel_values)
+        return pixel_values
 
-        output = self.model.generate(**inputs, max_new_tokens=100)
+    def query_for_image(self, query: str, image: Image.Image) -> str:
 
-        return self.processor.decode(output[0], skip_special_tokens=True)
+        preprocessed_images = self._preprocess_image(image).to(torch.bfloat16).to(self.device)
+        generation_config = dict(max_new_tokens=1024, do_sample=False)
+
+        question = f"<image>\n{query}"
+        response = self.model.chat(self.tokenizer, preprocessed_images, question, generation_config)
+
+        return response
